e10a5d942e190cb3f61e44225c7f2d43956e7990
user <rahulshrestha102@gmail.com>
2026-01-18 10:39:58 +0545

    bubble sort

--- arraySort.c
+++ arraySort.c
@@ -1,15 +1,27 @@
 #include <stdio.h>
-
+void sort(int num[],int index);
 int main()
 {
-    int num[10] = {5, 6, 3, 2, 4, 0, 9, 7, 8, 1};
-    int i, j, temp;
-
+    int num[9] = {5, 6, 3, 2, 4, 9, 7, 8, 1};
+    int i;
+    sort(num,9);
     for (i = 0; i < 9; i++)
     {
-        for (j = i + 1; j < 10; j++)
+        printf("%d ", num[i]);
+    }
+
+    return 0;
+}
+
+void sort(int num[], int index)
+{
+int i, j, temp;
+
+    for (i = 0; i < index; i++)                                     
+    {
+        for (j = i + 1; j < index-1; j++)
         {
-            if (num[i] > num[j])
+            if (num[i] < num[j])
             {
                 temp = num[i];
                 num[i] = num[j];
@@ -18,10 +30,4 @@ int main()
         }
     }
 
-    for (i = 0; i < 10; i++)
-    {
-        printf("%d ", num[i]);
-    }
-
-    return 0;
 }
